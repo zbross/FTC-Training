f4d02dcad6fcb5f4e01626c1af224aff8f9120c2
Mr. Bross <32577977+zbross@users.noreply.github.com>
2017-12-05 14:17:42 -0500

    Added sample auton using gyro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrainingAuto.java
@@ -0,0 +1,226 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+/**
+ * This file illustrates the concept of driving a path based on encoder counts.
+ * The code is structured as a LinearOpMode
+ *
+ * The code REQUIRES that you DO have encoders on the wheels.
+ *
+ *  This code ALSO requires that the drive Motors have been configured such that a positive
+ *  power command moves them forwards, and causes the encoders to count UP.
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 48 inches
+ *   - Spin right for 12 Inches
+ *   - Drive Backwards for 24 inches
+ *   - Stop and close the claw.
+ *
+ *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ *  that performs the actual movement.
+ *  This methods assumes that each movement is relative to the last stopping place.
+ *  There are other ways to perform encoder based moves, but this method is probably the simplest.
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Training Autonomous", group="Linear Opmode")
+//@Disabled
+public class TrainingAuto extends LinearOpMode {
+
+    // Declare OpMode members.
+    // Notice that because I am using two motors on each side of the robot, they much be
+    // initialized individually.
+    private DcMotor leftDrive1 = null;
+    private DcMotor leftDrive2 = null;
+    private DcMotor rightDrive1 = null;
+    private DcMotor rightDrive2 = null;
+    private Servo armServo = null;
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                                                      (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive1  = hardwareMap.get(DcMotor.class, "left_drive_1");
+        leftDrive2 = hardwareMap.get(DcMotor.class, "left_drive_2");
+        rightDrive1 = hardwareMap.get(DcMotor.class, "right_drive_1");
+        rightDrive2 = hardwareMap.get(DcMotor.class, "right_drive_2");
+        armServo = hardwareMap.get(Servo.class, "arm_servo");
+
+        // Most robots need the motor(s) on one side to be reversed to drive forward.
+        // Reverse the motor that runs backwards when connected directly to the battery.
+        // This can only be determined with live testing, but is often the left side
+        // in a traditional tank drive setup. Make sure that both motors on the same side
+        // are driving in the same direction, or else there will be serious problems!
+        leftDrive1.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive2.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive1.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive2.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set the servo to its starting position, in this case, zero
+        armServo.setPosition(0);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        leftDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        leftDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0",  "Starting at %7d :%7d",
+                          leftDrive1.getCurrentPosition(),
+                          rightDrive1.getCurrentPosition());
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+
+        armServo.setPosition(0.5);
+        sleep(1000);     // pause for servos to move
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+    /*
+     *  Method to perfmorm a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        // We have to define targets for both of the motors on each side
+        int newLeft1Target;
+        int newRight1Target;
+        int newLeft2Target;
+        int newRight2Target;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position for each motor, and pass to motor controller
+            newLeft1Target = leftDrive1.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRight1Target = rightDrive1.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newLeft2Target = leftDrive2.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRight2Target = rightDrive2.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            leftDrive1.setTargetPosition(newLeft1Target);
+            rightDrive1.setTargetPosition(newRight1Target);
+            leftDrive2.setTargetPosition(newLeft2Target);
+            rightDrive2.setTargetPosition(newRight2Target);
+
+            // Turn On RUN_TO_POSITION
+            leftDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightDrive2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            leftDrive1.setPower(Math.abs(speed));
+            rightDrive1.setPower(Math.abs(speed));
+            leftDrive2.setPower(Math.abs(speed));
+            rightDrive2.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                   (runtime.seconds() < timeoutS) &&
+                   (leftDrive1.isBusy() && rightDrive1.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeft1Target,  newRight1Target);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                                            leftDrive1.getCurrentPosition(),
+                                            rightDrive1.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            leftDrive1.setPower(0);
+            rightDrive1.setPower(0);
+            leftDrive2.setPower(0);
+            rightDrive2.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            leftDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+}
